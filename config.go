eac4435ab7d5b860aa5c538f1dedc21acaf86f52
Daniel Domurad <ddomurad@gmail.com>
2025-08-05 09:46:47 +0200

    Ddomurad/extract response handlers to customizable functions (#289)
    * Extract response handling to customizable functions
    
    * Spelling. Adding more tests.
    
    * lint: remove double import
    
    * Update README.md

--- config.go
+++ config.go
@@ -16,6 +16,7 @@ package otelgqlgen
 
 import (
 	"github.com/99designs/gqlgen/graphql"
+	"github.com/vektah/gqlparser/v2/gqlerror"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/trace"
 )
@@ -26,17 +27,27 @@ type SpanKindSelectorFunc func(operationName string) trace.SpanKind
 
 // config is used to configure the mongo tracer.
 type config struct {
-	TracerProvider             trace.TracerProvider
-	Tracer                     trace.Tracer
-	ComplexityExtensionName    string
-	RequestVariablesBuilder    RequestVariablesBuilderFunc
-	ShouldCreateSpanFromFields FieldsPredicateFunc
-	SpanKindSelectorFunc       SpanKindSelectorFunc
+	TracerProvider                     trace.TracerProvider
+	Tracer                             trace.Tracer
+	ComplexityExtensionName            string
+	RequestVariablesBuilder            RequestVariablesBuilderFunc
+	ShouldCreateSpanFromFields         FieldsPredicateFunc
+	SpanKindSelectorFunc               SpanKindSelectorFunc
+	InterceptResponseResultHandlerFunc InterceptResponseResultHandlerFunc
+	InterceptFieldsResultHandlerFunc   InterceptFieldsResultHandlerFunc
 }
 
 // RequestVariablesBuilderFunc is the signature of the function
 // used to build the request variables attributes.
 type RequestVariablesBuilderFunc func(requestVariables map[string]interface{}) []attribute.KeyValue
+
+// InterceptResponseResultHandlerFunc is the signature of the function
+// used to intercept and handle GraphQL response results before they are returned.
+type InterceptResponseResultHandlerFunc func(resp *graphql.Response, span trace.Span) *graphql.Response
+
+// InterceptFieldsResultHandlerFunc is the signature of the function
+// used to intercept and handle GraphQL field-level results and errors during execution.
+type InterceptFieldsResultHandlerFunc func(resp interface{}, respErr error, fieldErrList gqlerror.List, span trace.Span) (interface{}, error)
 
 // Option specifies instrumentation configuration options.
 type Option interface {
@@ -93,5 +104,21 @@ func WithCreateSpanFromFields(predicate FieldsPredicateFunc) Option {
 func WithSpanKindSelector(spanKindSelector SpanKindSelectorFunc) Option {
 	return optionFunc(func(cfg *config) {
 		cfg.SpanKindSelectorFunc = spanKindSelector
+	})
+}
+
+// WithInterceptResponseResultHandlerFunc allows specifying a custom function
+// to intercept and handle GraphQL response results before they are returned.
+func WithInterceptResponseResultHandlerFunc(handler InterceptResponseResultHandlerFunc) Option {
+	return optionFunc(func(cfg *config) {
+		cfg.InterceptResponseResultHandlerFunc = handler
+	})
+}
+
+// WithInterceptFieldsResultHandlerFunc allows specifying a custom function
+// to intercept and handle GraphQL field-level results and errors during execution.
+func WithInterceptFieldsResultHandlerFunc(handler InterceptFieldsResultHandlerFunc) Option {
+	return optionFunc(func(cfg *config) {
+		cfg.InterceptFieldsResultHandlerFunc = handler
 	})
 }
